0ebf5a6a36c46a65fb20e606f10a6ace5dc3bfad
Viviane Viana <vivianeviana59@gmail.com>
2023-02-13 01:01:16 -0300

    retrieving heroes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from utils import constants as const
 import requests
 
-url = const.BASE_URL
-
+heroes_url = const.HEROES_URL
 
 headers = {
 	"api_key": f"{const.APY_KEY}",
-	"players": "214372155"
 }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", heroes_url, headers=headers)
+content = response.json()
 
-print(response.text)
+
+hero_id = content[0]['id']
+hero_name = content[0]['localized_name']
+hero_attack_type = content[0]['attack_type']
+hero_roles = content[0]['roles']
+
+print(f'{hero_id} - {hero_name} - {hero_attack_type} - {hero_roles}')
--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,3 @@
 APY_KEY = "645ca35c-1ccb-46cd-9bc6-0f6748b5636b"
-BASE_URL = "https://api.opendota.com/api/"
+PLAYER_URL = "https://api.opendota.com/api/players"
+HEROES_URL = "https://api.opendota.com/api/heroes"
