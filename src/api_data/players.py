2d157619756845042b861c7910bed141f01f1ec6
Viviane Viana <vivianeviana59@gmail.com>
2023-02-14 00:47:54 -0300

    retrieving player infos

---
+++ src/api_data/players.py
@@ -0,0 +1,36 @@
+from src.utils import constants as const
+import requests
+
+headers = {
+    "api_key": f"{const.APY_KEY}"
+}
+
+account_id = input("Enter with the account id: ")
+
+players_url = f"https://api.opendota.com/api/players/{account_id}"
+
+response_player = requests.request("GET", players_url, headers=headers)
+player = response_player.json()
+
+player_account_id = player['profile']['account_id']
+player_personaname = player['profile']['personaname']
+
+print(f'{player_account_id} - {player_personaname}')
+
+########################## HEROES ##########################
+
+player_hero_url = f"https://api.opendota.com/api/players/{account_id}/heroes"
+
+response_player_heroes = requests.request("GET", player_hero_url, headers=headers)
+player_heroes = response_player_heroes.json()
+
+for hero in player_heroes:
+    if hero.get('games') != 0:
+        hero_id = hero.get('hero_id')
+        games = hero.get('games')
+        win = hero.get('win')
+
+        print(f'Hero_id: {hero_id} - Games: {games} - Wins: {win}')
+
+
+
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,3 @@
 APY_KEY = "645ca35c-1ccb-46cd-9bc6-0f6748b5636b"
-PLAYER_URL = "https://api.opendota.com/api/players"
+PLAYERS_URL = "https://api.opendota.com/api/players/{account_id}/heroes"
 HEROES_URL = "https://api.opendota.com/api/heroes"
